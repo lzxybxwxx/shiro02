2534bd5f9337b601dc8fa7944a813b5e74b55941
lzxybxwxx <2121797152@qq.com>
2021-12-11 15:37:03 +0800

    测试

--- src/main/java/com/zking/controller/indexCotrller.java
+++ src/main/java/com/zking/controller/indexCotrller.java
@@ -4,5 +4,6 @@ public class indexCotrller {
     public static void main(String[] args) {
         System.out.println("hdoighru");
         System.out.println("jdoighyruy");
+        System.out.println("hdiofheut");
     }
 }
